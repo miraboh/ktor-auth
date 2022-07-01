d333ea6580622b4e67ea9a6b08e9d0a1ec651766
mac <mac@macs-MacBook-Pro.local>
2022-07-01 15:19:53 -0500

    removed conf

--- src/main/kotlin/com/bayoku/Application.kt
+++ src/main/kotlin/com/bayoku/Application.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.*
 import com.bayoku.plugins.*
 
 fun main(args: Array<String>): Unit =
-        io.ktor.server.netty.EngineMain.main(args)
+    io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
